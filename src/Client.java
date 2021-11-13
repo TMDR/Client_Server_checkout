226eb1835d0d7014e833aaa9b2b01898e2bb95b9
TMDR <tmdrforgit@gmail.com>
2021-11-13 13:42:01 +0200

    first

---
+++ src/Client.java
@@ -0,0 +1,63 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        int choice = 1;Scanner sc;
+        do{
+            System.out.println("You can choose between TCP and UDP but we advise you to choose TCP as it provides 3 way handshake and every paccket is ensured to be delivered. Press 1 for TCP and 2 for UDP");
+            sc = new Scanner(System.in);
+            choice = sc.nextInt();
+        }while(choice < 1 || choice > 2);
+        sc.close();
+        switch(choice){
+            case 1:
+                try{
+                    while(true){
+                        Socket s = new Socket(args[0],Integer.parseInt(args[1]));
+                        s.setSoTimeout(Integer.parseInt(args[3]));
+                        System.out.println(s.getInetAddress().toString()+" is connected");
+                        s.close();
+                        Thread.sleep(Integer.parseInt(args[2]));
+                    }
+                }
+                catch(SocketTimeoutException ex){
+                    System.out.println("Server is down");
+                }
+                catch(SocketException e){
+                    System.out.println("Network is down");
+                }
+                break;
+            case 2:
+                DatagramSocket ds = new DatagramSocket();
+                ds.setSoTimeout(Integer.parseInt(args[3]));
+                try{
+                    while(true){
+                        byte b[] = new byte[1];
+                        b[0] = (byte)'t';
+                        DatagramPacket dp = new DatagramPacket(b, b.length,InetAddress.getByName(args[0]),Integer.parseInt(args[1]));
+                        ds.send(dp);
+                        dp = new DatagramPacket(dp.getData(), dp.getData().length);
+                        ds.receive(dp);
+                        InetAddress address = dp.getAddress();
+                        System.out.println("Server with address : "+address.toString()+" is UP and connected to me right now");
+                        Thread.sleep(Integer.parseInt(args[2]));
+                    }
+                }
+                catch(SocketTimeoutException ex){
+                    System.out.println("Server is down");
+                    ds.close();
+                }
+                catch(SocketException e){
+                    System.out.println("Network is down");
+                    ds.close();
+                }
+                break;
+        }
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,70 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        int choice = 1;Scanner sc;
+        do{
+            System.out.println("You can choose between TCP and UDP but we advise you to choose TCP as it provides 3 way handshake and every paccket is ensured to be delivered. Press 1 for TCP and 2 for UDP");
+            sc = new Scanner(System.in);
+            choice = sc.nextInt();
+        }while(choice < 1 || choice > 2);
+        sc.close();
+
+        switch(choice){
+            case 1:
+                ServerSocket ss = new ServerSocket(Integer.parseInt(args[0]));//this wwaits for clietn connections
+                try{
+                    while(true){
+                        ss.setSoTimeout(Integer.parseInt(args[2]));
+                        Socket s = ss.accept();//we wait for only one client
+                        s.setSoTimeout(Integer.parseInt(args[2]));
+                        s.close();
+                        System.out.println(s.getInetAddress().toString()+" is connected");
+                        Thread.sleep(Integer.parseInt(args[1]));
+                    }
+                }
+                
+                catch(SocketTimeoutException ex){
+                    System.out.println("Client is down ");
+                    ss.close();
+                }
+                catch(SocketException e){
+                    System.out.println("Network is down");
+                    ss.close();
+                }
+                break;
+            case 2:
+                DatagramSocket ds = new DatagramSocket(Integer.parseInt(args[0]));
+                ds.setSoTimeout(Integer.parseInt(args[2]));
+                try{
+                    while(true){
+                        byte b[] = new byte[1];
+                        DatagramPacket dp = new DatagramPacket(b, b.length);
+                        ds.receive(dp);
+                        InetAddress address = dp.getAddress();
+                        int port = dp.getPort();
+                        System.out.println("client with address : "+address.toString()+" is UP and connected to me right now");
+                        dp = new DatagramPacket(dp.getData(), dp.getData().length, address, port);
+                        ds.send(dp);
+                        Thread.sleep(Integer.parseInt(args[1]));
+                    }
+                }
+                catch(SocketTimeoutException ex){
+                    System.out.println("Client is down");
+                    ds.close();
+                }
+                catch(SocketException e){
+                    System.out.println("Network is down");
+                    ds.close();
+                }
+                break;
+        }
+    }
+}
